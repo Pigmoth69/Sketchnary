b23a349436263af661e133acdc521bcd3eb33efd
FilipaRamos <fimamora@hotmail.com>
2016-04-20 00:17:22 +0100

    database and insert example

---
+++ database.sql
@@ -0,0 +1,49 @@
+﻿DROP TABLE IF EXISTS Friend;
+DROP TABLE IF EXISTS Player;
+
+--- PLAYER ---
+CREATE TABLE Player(
+    id integer NOT NULL,
+    username text NOT NULL,
+    password text NOT NULL,
+    name text NOT NULL,
+    email text NOT NULL,
+    age integer NOT NULL,
+    country text,
+    CONSTRAINT "valid age" CHECK (age > 0)
+);
+
+--- PLAYER SEQUENCE ---
+CREATE SEQUENCE Player_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+--- PLAYER SEQUENCE CONSTRAINTS---
+ALTER SEQUENCE Player_id_seq OWNED BY Player.id;
+ALTER TABLE ONLY Player ALTER COLUMN id SET DEFAULT nextval('Player_id_seq'::regclass);
+SELECT pg_catalog.setval('Player_id_seq', 1, false);
+
+--- FRIEND ---
+CREATE TABLE Friend(
+	id_player integer NOT NULL,
+	id_friend integer NOT NULL
+);
+
+--- CONSTRAINTS ---
+ALTER TABLE ONLY Player
+       ADD CONSTRAINT "username unique" UNIQUE (username);
+ALTER TABLE ONLY Player
+	ADD CONSTRAINT "email unique" UNIQUE (email);
+ALTER TABLE ONLY Player
+	ADD CONSTRAINT "Player key" PRIMARY KEY (id);
+ALTER TABLE ONLY Friend
+	ADD CONSTRAINT id_player FOREIGN KEY (id_player) REFERENCES Player(id) MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE ONLY Friend
+	ADD CONSTRAINT id_friend FOREIGN KEY (id_friend) REFERENCES Player(id) MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+
+--- INDEX ---
+CREATE INDEX index_player_username ON Player USING hash(username);
+CREATE INDEX index_join_player_friend ON Friend USING btree(id_friend);
---
+++ insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO Player (username, password, name, email, age, country) VALUES ('hello123', 'idontknow', 'jorge joao', 'jj@gmail.com', 15, 'Portugal');
+INSERT INTO Player (username, password, name, email, age, country) VALUES ('b3stUs3rnam3', 'passpass333', 'rita sofia', 'ritinha@gmail.com', 22, 'Portugal');
+INSERT INTO Player (username, password, name, email, age, country) VALUES ('GGG', 'sdgj', 'jonas', 'jonas@gmail.com', 53, 'Portugal');
+INSERT INTO Player (username, password, name, email, age, country) VALUES ('faltas', 'eeu:(', 'a esquecida', 'esquecida@gmail.com', 19, 'Portugal');
+INSERT INTO Player (username, password, name, email, age, country) VALUES ('marianela', 'mariazita444', 'maria manuela', 'hello@gmail.com', 17, 'Portugal');
+INSERT INTO Player (username, password, name, email, age, country) VALUES ('x321x', 'shvine', 'centeio felgueiras', 'centeiof@gmail.com', 49, 'Portugal');
+INSERT INTO Player (username, password, name, email, age, country) VALUES ('eutb', 'idontknow', 'tomás guarda', 'tomasg@gmail.com', 12, 'Portugal');
+
+INSERT INTO Friend (id_player, id_friend) VALUES (1, 2);
+INSERT INTO Friend (id_player, id_friend) VALUES (1, 5);
+INSERT INTO Friend (id_player, id_friend) VALUES (1, 7);
+
+INSERT INTO Friend (id_player, id_friend) VALUES (2, 1);
+INSERT INTO Friend (id_player, id_friend) VALUES (2, 4);
+
+INSERT INTO Friend (id_player, id_friend) VALUES (3, 1);
+INSERT INTO Friend (id_player, id_friend) VALUES (3, 7);
+INSERT INTO Friend (id_player, id_friend) VALUES (3, 5);
+INSERT INTO Friend (id_player, id_friend) VALUES (3, 2);
+INSERT INTO Friend (id_player, id_friend) VALUES (3, 6);
+INSERT INTO Friend (id_player, id_friend) VALUES (3, 4);
