42c1ad454b81dbcf34873c51d71cd16f463bd907
FilipaRamos <fimamora@hotmail.com>
2016-05-12 16:46:35 +0100

    SSLContext and professor keys

--- src/https/HttpsServer.java
+++ src/https/HttpsServer.java
@@ -2,27 +2,11 @@ package https;
 
 import server.*;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetSocketAddress;
 import java.security.KeyStore;
 
-import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLServerSocket;
-import javax.net.ssl.SSLServerSocketFactory;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-
-import com.sun.net.httpserver.HttpsConfigurator;
 
 public class HttpsServer {
 
@@ -37,39 +21,36 @@ public class HttpsServer {
 	}
 
 	private SSLContext createSSLContext() {
+		
+		SSLContext sslContext = null;
+		
 		try {
-			KeyStore keyStore = KeyStore.getInstance("JKS");
-			keyStore.load(new FileInputStream("test.jks"), "passphrase".toCharArray());
+			sslContext = SSLContext.getInstance("TLS");
+			char[] keystorePassword = "123456".toCharArray();
+			KeyStore ks = KeyStore.getInstance("JKS");
 
-			// Create key manager
-			KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
-			keyManagerFactory.init(keyStore, "passphrase".toCharArray());
-			KeyManager[] km = keyManagerFactory.getKeyManagers();
+			ks.load(new FileInputStream("keys/server.keys"), keystorePassword);
 
-			// Create trust manager
-			TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
-			trustManagerFactory.init(keyStore);
-			TrustManager[] tm = trustManagerFactory.getTrustManagers();
+			KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+			kmf.init(ks, keystorePassword);
+			sslContext.init(kmf.getKeyManagers(), null, null);
 
-			// Initialize SSLContext
-			SSLContext sslContext = SSLContext.getInstance("TLSv1");
-			sslContext.init(km, tm, null);
-
-			return sslContext;
-		} catch (Exception ex) {
-			ex.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
-		return null;
+		return sslContext;
 	}
 
 	// Start to run the server
 	public void run() {
-		//HttpsServer httpserver = HttpsServer.create(new InetSocketAddress(443), 0);
-		//httpserver.setHttpsConfigurator(new HttpsConfigurator(createSSLContext()));
-		//httpserver.createContext("/api", new API());
-		//httpserver.setExecutor(null);
-		//httpserver.start();
+		// HttpsServer httpserver = HttpsServer.create(new
+		// InetSocketAddress(443), 0);
+		// httpserver.setHttpsConfigurator(new
+		// HttpsConfigurator(createSSLContext()));
+		// httpserver.createContext("/api", new API());
+		// httpserver.setExecutor(null);
+		// httpserver.start();
 	}
 
 	public void httpsServer() {
